e935b3f9f3a3def97b25286157780bd016cde51b
Andres Calderon <andres.calderon@admobilize.com>
2017-09-27 21:02:32 +0000

    audio widgets support added matrixio regmap connected to the codec

--- matrixio-codec.c
+++ matrixio-codec.c
@@ -26,6 +26,8 @@
 #define MATRIXIO_RATES         SNDRV_PCM_RATE_8000_48000
 #define MATRIXIO_FORMATS       SNDRV_PCM_FMTBIT_S16_LE
 
+static struct matrixio* matrixio;
+
 static int matrixio_codec_hw_params(struct snd_pcm_substream *substream, struct snd_pcm_hw_params *params, struct snd_soc_dai *dai) {
 
 	return 0;
@@ -35,29 +37,63 @@ static const struct snd_soc_dai_ops matrixio_dai_ops = {
 	.hw_params = matrixio_codec_hw_params,
 };
 
-static  struct snd_soc_dai_driver matrixio_dai_driver = {
-	.name = "matrixio-codec",
-	.capture = {
-		.stream_name = "Capture",
-		.channels_min = 1,
-		.channels_max = MATRIXIO_CHANNELS_MAX,
-		.rates = MATRIXIO_RATES,
-		.formats = MATRIXIO_FORMATS,
-	},
-	.ops = &matrixio_dai_ops,
+static const struct snd_kcontrol_new matrixio_snd_controls[] = {
 };
 
+static const struct snd_soc_dapm_widget matrixio_dapm_widgets[] = {
+};
+
+static const struct snd_soc_dapm_route matrixio_dapm_routes[] = {
+};
+
+static int matrixio_add_widgets(struct snd_soc_codec *codec) {
+	struct snd_soc_dapm_context *dapm = snd_soc_codec_get_dapm(codec);
+
+	snd_soc_add_codec_controls(codec, matrixio_snd_controls,
+			ARRAY_SIZE(matrixio_snd_controls));
+
+	snd_soc_dapm_new_controls(dapm, matrixio_dapm_widgets,
+			ARRAY_SIZE(matrixio_dapm_widgets));
+
+	snd_soc_dapm_add_routes(dapm, matrixio_dapm_routes, 
+			ARRAY_SIZE(matrixio_dapm_routes));
+
+	return 0;
+}
+
+
+static int matrixio_probe(struct snd_soc_codec *codec) {
+
+	dev_set_drvdata(codec->dev, matrixio);
+
+	matrixio_add_widgets(codec);
+
+	snd_soc_codec_init_regmap(codec, matrixio->regmap);
+
+	return 0;
+}
+
 static const struct snd_soc_codec_driver matrixio_soc_codec_driver = {
+
+	.probe = matrixio_probe,
 /*
-	.probe =
 	.set_bias_level =
 	.read =
 	.write =
 */
 };
 
-
-static struct matrixio* matrixio;
+static  struct snd_soc_dai_driver matrixio_dai_driver = {
+        .name = "matrixio-codec",
+        .capture = {
+                .stream_name = "Capture",
+                .channels_min = 1,
+                .channels_max = MATRIXIO_CHANNELS_MAX,
+                .rates = MATRIXIO_RATES,
+                .formats = MATRIXIO_FORMATS,
+        },
+        .ops = &matrixio_dai_ops,
+};
 
 static int matrixio_codec_probe(struct platform_device *pdev)
 {
