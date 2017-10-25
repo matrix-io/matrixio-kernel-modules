7df5aef3ca9be66f0b23ff36507b9d8b1d0060e7
Kevin Patino <kevindpatino@gmail.com>
2017-10-25 21:22:06 +0000

    GPIO kernel has been completed

--- matrixio-gpio.c
+++ matrixio-gpio.c
@@ -4,39 +4,89 @@
 #include <linux/init.h>
 #include <linux/module.h>
 #include <linux/platform_device.h>
+#include <linux/bitops.h>
 
 #include "matrixio-core.h"
+
+#define MATRIXIO_GPIO_BASE 0x2800
 
 struct matrixio_gpio {
 	struct gpio_chip chip;
 	struct matrixio *mio;
 };
 
-static int matrixio_gpio_get_direction(struct gpio_chip *chip, unsigned offset)
+static int matrixio_gpio_get_direction(struct gpio_chip *gc, unsigned offset)
 {
-	return 0;
+	struct matrixio_gpio *chip = gpiochip_get_data(gc);
+	int gpio_direction;
+
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE, &gpio_direction);
+
+	return (gpio_direction & BIT(offset));
 }
 
-static int matrixio_gpio_direction_input(struct gpio_chip *chip,
+static int matrixio_gpio_direction_input(struct gpio_chip *gc,
 					 unsigned offset)
 {
-	return -EINVAL;
+	struct matrixio_gpio *chip = gpiochip_get_data(gc);
+	int gpio_direction;
+
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE, &gpio_direction);
+	
+	gpio_direction |= BIT(offset);
+
+	regmap_write(chip->mio->regmap, MATRIXIO_GPIO_BASE, gpio_direction);
+
+	return 0;
+
 }
 
-static int matrixio_gpio_direction_output(struct gpio_chip *chip,
+static int matrixio_gpio_direction_output(struct gpio_chip *gc,
 					  unsigned offset, int value)
 {
+	struct matrixio_gpio *chip = gpiochip_get_data(gc);
+	int gpio_direction;
+	int gpio_value;
+
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE, &gpio_direction);
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE + 1, &gpio_value);
+	
+	if(value)
+		gpio_value |= BIT(offset);
+	else 
+		gpio_value &= ~BIT(offset);
+
+	regmap_write(chip->mio->regmap, MATRIXIO_GPIO_BASE, gpio_direction);
+	regmap_write(chip->mio->regmap, MATRIXIO_GPIO_BASE + 1, gpio_value);
+
 	return 0;
 }
 
-static int matrixio_gpio_get(struct gpio_chip *chip, unsigned offset)
+static int matrixio_gpio_get(struct gpio_chip *gc, unsigned offset)
 {
-	return 0;
+	struct matrixio_gpio *chip = gpiochip_get_data(gc);
+	int gpio_value;
+
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE + 1, &gpio_value);
+
+	return (gpio_value & BIT(offset));
 }
 
-static void matrixio_gpio_set(struct gpio_chip *chip, unsigned offset,
+static void matrixio_gpio_set(struct gpio_chip *gc, unsigned offset,
 			      int value)
 {
+	struct matrixio_gpio *chip = gpiochip_get_data(gc);
+	int gpio_value;
+
+	regmap_read(chip->mio->regmap, MATRIXIO_GPIO_BASE + 1, &gpio_value);
+	
+	if(value)
+		gpio_value |= BIT(offset);
+	else 
+		gpio_value &= ~BIT(offset);
+
+	regmap_write(chip->mio->regmap, MATRIXIO_GPIO_BASE + 1, gpio_value);
+
 }
 
 static const struct gpio_chip matrixio_gpio_chip = {
